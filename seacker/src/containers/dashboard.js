534edc15b8d7bdcfdcf22e7c7fd14e9855f1b71a
nvtrakhmawati <rakhmawati.novita@gmail.com>
2019-08-27 15:24:48 +0700

    half done

--- seacker/src/containers/dashboard.js
+++ seacker/src/containers/dashboard.js
@@ -1,44 +1,23 @@
 import React, {Component} from 'react'  
 import { Button } from 'react-bootstrap'
 
-const position = [
-    {
-        x : 640,
-        y : 255
-    },
-    {
-        x : 665,
-        y : 255
-    },
-    {
-        x : 690,
-        y : 255
-    },
-    {
-        x : 715,
-        y : 255
-    },
-    {
-        x : 735,
-        y : 255
-    }
-
-]
 
 class Dashboard extends Component {
+    componentDidMount(){
+
+    }
+    
     render() {
       return( 
         <div style={{marginTop:'30px'}}>
             <img src={require("../assets/landingPage.png")} style={{width:'1350px', height:'625px'}}></img>
-            {/* {
+            {
                 position.map(pos => {
                     return(
                         <Button style={{position:'absolute', left:pos.x, top:pos.y, zIndex:1, width:1, height:1, margin:5, padding:5, backgroundColor:'green'}}></Button>
                     )
                 })
-            } */}
-            <Button style={{position:'absolute', left:640, top:300, zIndex:1, width:1, height:1, margin:5, padding:5, backgroundColor:'green'}}></Button>
-            <Button style={{position:'absolute', left:665, top:300, zIndex:1, width:1, height:1, margin:5, padding:5, backgroundColor:'green'}}></Button>
+            }
         </div>
       )
     }
