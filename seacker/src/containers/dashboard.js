1f78b45556fd0c94f938269fd3ae308aa1d1bd5a
nvtrakhmawati <rakhmawati.novita@gmail.com>
2019-08-28 16:20:09 +0700

    testing

--- seacker/src/containers/dashboard.js
+++ seacker/src/containers/dashboard.js
@@ -1,78 +1,97 @@
-import React, {useState, useEffect} from 'react'  
+import React, { useState, useEffect } from 'react'
 import { Button } from 'react-bootstrap'
 import { connect } from 'react-redux'
-import { fetchSeats } from '../stores/actions'
+import { fetchSeats, detailSeat, bookSeat } from '../stores/actions'
 import Modal from 'react-bootstrap/Modal'
 import Spinner from 'react-bootstrap/Spinner'
 
-function Dashboard (props){
+function Dashboard(props) {
     const [show, setShow] = useState(false);
+    const [ID, setId] = useState('')
+    const [data, setData] = useState([])
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const handleClose = (id) => {
+        // console.log(id, 'mau book nih')
+        // e.preventDefault()
+        setShow(false)
+        props.bookSeat(id)
+    };
 
-    useEffect(()=> {
+    const handleShow = (id) => {
+        setShow(true);
+        props.detailSeat(id)
+        setId(id)
+    }
+
+    const fetch = () => {
         props.fetchSeats()
+    }
 
-        if(!localStorage.getItem('token')){
+    useEffect(() => {
+        fetch()
+        console.log(props)
+        if (!localStorage.getItem('token')) {
             props.history.push('/')
         }
-    }, [])
-    
-    return( 
-        <div style={{marginTop:'30px'}}>
+
+    },[])
+
+    return (
+        <div style={{ marginTop: '30px' }}>
             {
-                props.isLoading ? (
-                    <h3 style={{textAlign : 'center'}}>Loading ...</h3>
-                ) : (
-                    props.allSeat ? (
-                        <div>
-                               <img src={require("../assets/landingPage.png")} style={{width:'1350px', height:'625px'}} alt='denah'></img>
-                {
-                props.allSeat.map((pos, index) => {
-                    return(
-                        <>
-                        <Button style={{position:'absolute', left:pos.coorX, top:pos.coorY, zIndex:1, 
-                            width:1, height:1, margin:5, padding:5, backgroundColor:'green'}}
-                            key={index} onClick={handleShow}
-                        ></Button>
-                        <Modal show={show} onHide={handleClose}>
-                            <Modal.Header closeButton>
-                            <Modal.Title>Modal heading</Modal.Title>
-                            </Modal.Header>
-                            <Modal.Body>coba</Modal.Body>
-                            <Modal.Footer>
-                            <Button variant="secondary" onClick={handleClose}>
-                                Close
-                            </Button>
-                            <Button variant="primary" onClick={handleClose}>
-                                Save Changes
-                            </Button>
-                            </Modal.Footer>
-                        </Modal>
-                        </>
-                    )
-                })
-            }
-                        </div>
-                    ) : (<div>
-                        <h6>error</h6>
+                !props.allSeat.isLoading && props.allSeat.data && (
+                    <div>
+                        <img src={require("../assets/landingPage.png")} style={{ width: '1350px', height: '625px' }} alt='denah'></img>
+                        {
+                            props.allSeat.data.map((pos, index) => (
+                                <>
+                                <Button style={{
+                                    position: 'absolute', left: pos.coorX, top: pos.coorY, zIndex: 1,
+                                    width: 1, height: 1, margin: 5, padding: 5, backgroundColor: 'green'
+                                }}
+                                    key={index} onClick={() => handleShow(pos._id)}
+                                ></Button>
+                                {
+                                    props.seatDetail.isLoading ? (
+                                        <div></div>
+                                    ) : (
+                                        props.seatDetail.detail && props.seatDetail.detail.blockName && (
+                                            <Modal show={show} onHide={handleClose} >
+                                                <Modal.Header closeButton>
+                                                    <Modal.Title>Booking Seat</Modal.Title>
+                                                </Modal.Header>
+                                                    <Modal.Body>Do you want to check in to seat {props.seatDetail.detail.blockName.name}-{props.seatDetail.detail.index}</Modal.Body>
+                                                    <Modal.Footer>
+                                                        <Button variant="secondary" onClick={(e) => handleClose(e)}>
+                                                            Close
+                                                        </Button>
+                                                        <Button variant="primary" onClick={() => handleClose(props.seatDetail.detail._id)}>
+                                                        Yes
+                                                        </Button>
+                                                    </Modal.Footer>
+                                            </Modal>
+                                        )
+                                    )
+
+                                }
+                                </>
+
+                            ))
+                        }
                     </div>
                 )
-                    // <h6>{JSON.stringify(props.allSeat)}</h6>
-                )
             }
-         
+            
+
         </div>
-      )
-    }
+    )
+}
 const mapStateToProps = state => {
-    return{
-        ...state,
-        allSeat : state.allSeat
+    return {
+       ...state
     }
 }
-const mapDispatchToProps = { fetchSeats }
+const mapDispatchToProps = { fetchSeats, detailSeat, bookSeat }
 
 // export default Dashboard
-export default connect(mapStateToProps,mapDispatchToProps)(Dashboard)
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
